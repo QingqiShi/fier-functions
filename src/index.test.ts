070cb6c83047dc83fcc70c57888fc5305b454f97
Qingqi Shi <qingqishi@live.com>
2019-10-11 18:36:22 +0100

    feat: initial commit

---
+++ src/index.test.ts
@@ -0,0 +1,16 @@
+const test = require('firebase-functions-test')();
+
+import { helloWorld } from './index';
+
+it('helloWorld', () => {
+  const req = null;
+  const res = {
+    send: jest.fn()
+  };
+
+  helloWorld(req as any, res as any);
+
+  expect(res.send).toHaveBeenCalledWith('Hello world');
+});
+
+test.cleanup();
---
+++ src/index.ts
@@ -0,0 +1,10 @@
+import * as functions from 'firebase-functions';
+
+// // Start writing Firebase Functions
+// // https://firebase.google.com/docs/functions/typescript
+//
+export const helloWorld = functions
+  .region('europe-west2')
+  .https.onRequest((_request, response) => {
+    response.send('Hello world');
+  });
